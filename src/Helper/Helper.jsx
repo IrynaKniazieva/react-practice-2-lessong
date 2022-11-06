e8a413524ca866f0c5960ea647f242efc48e1f3a
IrynaKniazieva <gleb0812@gmail.com>
2022-11-06 17:51:06 +0200

    lessong

---
+++ src/Helper/Helper.jsx
@@ -0,0 +1,11 @@
+export const helper = array => {
+    return array.map(
+      ({ backdrop_path: image, id, title, vote_count: votes }) => ({
+        image,
+        id,
+        title,
+        votes,
+      })
+    );
+  };
+  
